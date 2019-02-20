4e088623b762e8b256dad327fc33ccf7f820249a
EvangelineHuang <47224681+EvangelineHuang@users.noreply.github.com>
2019-02-20 16:13:22 -0500

    Update Array.js

--- Array.js
+++ Array.js
@@ -6,38 +6,33 @@ e.appendChild(list);
 var listelement = document.createElement("li");
 var node = createTextNode("dog");
 listelement.appendChild(node);
-var element = document.getElementByTagName("ul");
-element.appendChild(listelement);
+list.appendChild(listelement);
 
 
 var listelement1 = document.createElement("li");
 var node1 = createTextNode("cat");
-listelement1.appendChild(node1);
-element.appendChild(listelement1);
+list.appendChild(listelement1);
 
 
 var listelement2 = document.createElement("li");
 var node2 = createTextNode("hippo");
-listelement2.appendChild(node2);
-element.appendChild(listelement2);
+list.appendChild(listelement2);
 
 
 var listelement3 = document.createElement("li");
 var node3 = createTextNode("bat");
-listelement3.appendChild(node3);
-element.appendChild(listelement3);
+list.appendChild(listelement3);
 
 
 var listelement4 = document.createElement("li");
 var node4 = createTextNode("alligator");
-listelement4.appendChild(node4);
-element.appendChild(listelement4);
+list.appendChild(listelement4);
 
 
 var listelement5 = document.createElement("li");
 var node5 = createTextNode("lion");
 listelement5.appendChild(node5);
-element.appendChild(listelement5);
+list.appendChild(listelement5);
 
 
 
