846c779bffae9a10aa0d0d5786a9fc56eb537a65
rob102910 <47224682+rob102910@users.noreply.github.com>
2019-02-20 15:39:55 -0500

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,8 +1,12 @@
 var L= ["dog","cat","Hippo","Bat","Alligator", "Lion"];
 var list = document.createElement("ul");
+var e = document.getElementByTagName("body");
+e.appendChild(list);
 L.forEach(function(d);
 {
-  var listelement = document.createElement("il");
+  var listelement = document.createElement("li");
   var node = createTextNode(d);
   listelement.appendChild(node);
+  var element = document.getElementByTagName("ul");
+  element.appendChild(listelement);
 })
